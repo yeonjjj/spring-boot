7b8b18e3539dff751c663ee2aac5c5ac73965ef4
YYJ <hidua1124@naver.com>
2020-05-29 21:08:11 +0900

    6.1 파일 첨부를 위한 기본 설정
    - build.gradle 파일에 commons-io, commons-fileupload 라이브러리 추가
    - WebMvcConfiguration 클래스에 아파치의 CommonsMultipartResolver를 이용하여 multipartResolver 구현
    - BoardApplication 클래스의 @SpringBootApplication 어노테이션을 수정하여 자동구성을 사용하지 않도록 처리

--- board/src/main/java/board/configuration/WebMvcConfiguration.java
+++ board/src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,7 +1,9 @@
 package board.configuration;
 
 import board.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +14,14 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new LoggerInterceptor());
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setDefaultEncoding("UTF-8");   //파일의 인코딩 설정
+        commonsMultipartResolver.setMaxUploadSizePerFile(5*1024*1024);  //바이트 단위, 5MB
+        return commonsMultipartResolver;
     }
 
 }
