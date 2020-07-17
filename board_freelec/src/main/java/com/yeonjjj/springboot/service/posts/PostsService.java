c532580c3a18e151ef62823be0de77e014e83210
YYJ <hidua1124@naver.com>
2020-07-18 03:56:03 +0900

    Posts 등록 API 만들기 및 테스트

---
+++ board_freelec/src/main/java/com/yeonjjj/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.yeonjjj.springboot.service.posts;
+
+import com.yeonjjj.springboot.domain.posts.PostsRepository;
+import com.yeonjjj.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
