1bcb27c071652eff9f09e08312e4b9a4a66810a5
YYJ <hidua1124@naver.com>
2020-08-14 02:15:48 +0900

    구글 로그인 연동하기(Spring Security, OAuth 2.0)

--- board_freelec/src/main/java/com/yeonjjj/springboot/web/IndexController.java
+++ board_freelec/src/main/java/com/yeonjjj/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yeonjjj.springboot.web;
 
+import com.yeonjjj.springboot.config.auth.dto.SessionUser;
 import com.yeonjjj.springboot.service.posts.PostsService;
 import com.yeonjjj.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {      // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체 저장
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
         // mustache starter에 의해 컨트롤러에서 문자열 반환 시 앞에 경로, 뒤에 파일 확장자가 붙어서 처리됨
         // src/main/resources/templates/index.mustache
         return "index";
